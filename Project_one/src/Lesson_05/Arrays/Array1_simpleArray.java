be74a60604f70df2b75166a76d13fa9c44e2f7b0
Undetka <roman_demenkov@mail.ru>
2022-07-04 18:33:21 +0300

    commit

---
+++ Project_one/src/Lesson_05/Arrays/Array1_simpleArray.java
@@ -0,0 +1,9 @@
+package Lesson_05.Arrays;
+
+public class Array1_simpleArray {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+    }
+}
---
+++ Project_one/src/Lesson_05/Random_class.java
@@ -0,0 +1,16 @@
+package Lesson_05;
+
+import java.util.Random;
+
+public class Random_class {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int  randomN = random.nextInt(100);
+        System.out.println("Random number " + randomN);
+
+        double randomN_double = random.nextDouble();
+        System.out.println("Random double " + randomN_double);
+
+    }
+}
